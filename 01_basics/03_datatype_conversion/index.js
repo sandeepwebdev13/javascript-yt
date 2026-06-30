e577aeeea16d9d603f28732cf9e9a42c3a136290
sandeepwebdev13 <sandeep@logicdigger.com>
2026-06-30 05:15:06 +0000

    conversion of datatpyes

---
+++ 01_basics/03_datatype_conversion/index.js
@@ -0,0 +1,34 @@
+console.log('Data type Conversion Operation');
+
+
+let score = 4;
+
+// console.log(score)
+// console.log(typeof score)
+
+let numToStr = String(score)
+// console.log(typeof numToStr)
+
+
+
+
+let numStr = "13y"
+
+// console.log(numStr)
+// console.log(typeof numStr)
+
+let strToNum = Number(numStr)
+
+// console.log(strToNum)
+// console.log(typeof strToNum)
+
+
+// let boolenCon = "0";
+let boolenCon = false;
+console.log(boolenCon)
+console.log(typeof boolenCon)
+
+
+let strToBool = Boolean(boolenCon)
+console.log(strToBool)
+console.log(typeof strToBool)
